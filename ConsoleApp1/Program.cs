6f85d75d41c1ede95f6d07ba4a7fb7b41bf15bfc
Titlehhhh <Titlehhhh@users.noreply.github.com>
2024-12-28 21:04:38 +0500

    add readme

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ public class Program
         RunBot();
     }
 
-    static void RunBot()
+    static async Task RunBot()
     {
         var packet = new ClientUseItemPacket._340_567(5, 6);
 
@@ -17,6 +17,16 @@ public class Program
 
 
         MinecraftProtocol protocol = new MinecraftProtocol();
+    
+        // Мульти версионная отправка
+        await protocol.SendUseItem.Send(hand: 0);
+        
+        // Определенная версия
+        if(protocol.SendUseItem.TrySend_759_766(out var sender))
+        {
+            await sender.Send(hand: 0, sequence: 0);
+        }
+        
 
         var observable = protocol.OnPacket<ChatMessagePacket>();
     }
