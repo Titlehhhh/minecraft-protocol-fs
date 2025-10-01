107caed26861cfd21ce655a186139e18c8b77bcf
Titlehhhh <Titlehhhh@users.noreply.github.com>
2025-10-01 20:16:48 +0500

    add F# project

---
+++ src/PacketGenerator/Program.fs
@@ -0,0 +1,37 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System
+open Humanizer
+open PacketGenerator.Core
+open Protodef
+open PacketGenerator.Extensions
+
+let protoMap = ProtocolLoader.LoadProtocolsAsync(735, 772).GetAwaiter().GetResult()
+
+
+
+
+let isPacket (s: string) = s.StartsWith("packet_") || s = "packet"
+
+let filterTypes = [| "native" |]
+
+
+
+let allTypes =
+    protoMap.Protocols
+    |> Seq.map _.Value.Protocol.GetAllTypes()
+    |> Seq.concat
+    |> Seq.filter (_.IsCustom(filterTypes) >> not)
+    |> Seq.map _.ParentName
+    |> Seq.filter (isPacket >> not)
+    |> Seq.map _.Pascalize()
+    |> Set
+
+
+
+let fType = protoMap.Protocols.Values |> Seq.head
+
+let comparer = PascalizeStringComparer.Instance
+
+let gg = fType.Protocol.TryFindTypeOption("Slot", comparer)
+
+printfn "%A" gg
